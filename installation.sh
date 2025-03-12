94591f0a6192c45ff2b7156932768ab8de92b198
TheodoreGillot <tgillot@albertschool.com>
2025-03-12 03:54:19 +0100

    updated user related cfg

--- installation.sh
+++ installation.sh
@@ -1,12 +1,32 @@
-mkdir -p ~/bin 
-[ -d "$HOME/.local/state/nvim" ] && rm -rf ~/.local/state/nvim
+#!/bin/bash
+
+USER_HOME=$(eval echo ~$SUDO_USER)
+BIN_DIR="$USER_HOME/bin"
+CONFIG_DIR="$USER_HOME/.config"
+STATE_DIR="$USER_HOME/.local/state"
+
+rm -rf "$STATE_DIR/nvim" "$CONFIG_DIR/nvim" "$BIN_DIR/nvim-linux-x86_64.appimage" "$BIN_DIR/nvim"
+
+mkdir -p "$CONFIG_DIR" "$BIN_DIR"
+
+cd "$BIN_DIR" || exit 1
+
 wget https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-linux-x86_64.appimage
 chmod u+x nvim-linux-x86_64.appimage
-ln -s ~/bin/nvim.appimage ~/bin/nvim 
-nvim --version
-[ ! -d "$HOME/.config" ] && mkdir -p "$HOME/.config"
-[ -d "$HOME/.config/nvim" ] && rm -rf "$HOME/.config/nvim"
-git clone https://github.com/NvChad/starter ~/.config/nvim 
-sudo apt update
-source ~/.bashrc 
-cd ~
+ln -sf "$BIN_DIR/nvim-linux-x86_64.appimage" "$BIN_DIR/nvim"
+
+chown -R "$SUDO_USER:$SUDO_USER" "$BIN_DIR"
+
+"$BIN_DIR/nvim" --version
+
+cd "$USER_HOME" || exit 1
+
+git clone https://github.com/NvChad/starter "$CONFIG_DIR/nvim"
+
+sudo rm -rf "$CONFIG_DIR/nvim/.git" "$CONFIG_DIR/nvim/.github"
+
+chown -R "$SUDO_USER:$SUDO_USER" "$CONFIG_DIR/nvim"
+
+sudo apt install -y fuse
+
+source "$USER_HOME/.bashrc"
