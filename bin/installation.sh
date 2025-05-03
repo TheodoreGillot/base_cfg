5d7a9d5833f67fae8b3cce17b8eb2b114254e078
TheodoreGillot <tgillot@albertschool.com>
2025-05-03 13:51:04 +0200

    update and bug fix

--- bin/installation.sh
+++ bin/installation.sh
@@ -5,7 +5,7 @@ BIN_DIR="$USER_HOME/bin"
 CONFIG_DIR="$USER_HOME/.config"
 STATE_DIR="$USER_HOME/.local/state"
 CONFIG_FILE="../cfg/config.ini"
-DATA_DIR="../${SERVICE}-data"
+DATA_DIR="../bash-data" #TODO change service adaptability
 OS="$(crudini "$CONFIG_FILE" cfg service)"
 
 arch=$(uname -m)
@@ -46,29 +46,30 @@ git config --global user.email "$EMAIL"
 git config --global user.name "$NAME"
 
 rm -rf "$STATE_DIR/nvim" "$CONFIG_DIR/nvim" "$BIN_DIR/nvim-linux-x86_64.appimage" "$BIN_DIR/nvim"
-
 mkdir -p "$CONFIG_DIR" "$BIN_DIR"
 
 cd "$BIN_DIR" || exit 1
 
-if ["$OS" = "linux-x86_64"]; then 
-  wget https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-linux-x86_64.appimage # ubuntu x86 
-  chmod u+x nvim-linux-x86_64.appimage
-  ls -sf "$BIN_DIR/nvim-linux-x86_64.appimage" "$BIN_DIR/nvim"
+if [[ "$OS" == "linux-x86_64" ]]; then
+    wget https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-linux-x86_64.appimage
+    chmod u+x nvim-linux-x86_64.appimage
+    ln -sf "$BIN_DIR/nvim-linux-x86_64.appimage" "$BIN_DIR/nvim"
 fi
 
-if ["$OS" = "mac-arm64"]; then 
-  https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-macos-arm64.tar.gz # mac arm
-  un nvim-macos-arm64.tar.gz 
-  chmod u+x nvim-macos-arm64
-  ln -sf "$BIN_DIR/nvim-macos-arm64" "$BIN_DIR/nvim"
+if [[ "$OS" == "mac-arm64" ]]; then
+    curl -LO https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-macos-arm64.tar.gz
+    tar -xzf nvim-macos-arm64.tar.gz
+    mv nvim-macos-arm64 "$BIN_DIR/"
+    chmod u+x "$BIN_DIR/nvim-macos-arm64"
+    ln -sf "$BIN_DIR/nvim-macos-arm64" "$BIN_DIR/nvim"
 fi
 
-if ["$OS" = "mac-x86_64"]; then
-  https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-macos-x86_64.tar.gz # mac x86 
-  un nvim-macos-x86_64.tar.gz
-  chmod u+x nvim-macos-x86_64
-  ln -sf "$BIN_DIR/nvim-macos-x86_64" "$BIN_DIR/nvim"
+if [[ "$OS" == "mac-x86_64" ]]; then
+    curl -LO https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-macos-x86_64.tar.gz
+    tar -xzf nvim-macos-x86_64.tar.gz
+    mv nvim-macos-x86_64 "$BIN_DIR/"
+    chmod u+x "$BIN_DIR/nvim-macos-x86_64"
+    ln -sf "$BIN_DIR/nvim-macos-x86_64" "$BIN_DIR/nvim"
 fi
 
 chown -R "$SUDO_USER:$SUDO_USER" "$BIN_DIR"
@@ -78,9 +79,7 @@ chown -R "$SUDO_USER:$SUDO_USER" "$BIN_DIR"
 cd "$USER_HOME" || exit 1
 
 git clone https://github.com/NvChad/starter "$CONFIG_DIR/nvim"
-
 sudo rm -rf "$CONFIG_DIR/nvim/.git" "$CONFIG_DIR/nvim/.github"
-
 chown -R "$SUDO_USER:$SUDO_USER" "$CONFIG_DIR/nvim"
 
 source "$USER_HOME/.bashrc"
